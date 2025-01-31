a523c603085c69f7ddf1a16ccb08728ddea826db
Sagar Joshi <coderjoshi15@gmail.com>
2025-01-31 22:05:30 +0530

    third commit

--- my-portfolio/src/Project.jsx
+++ my-portfolio/src/Project.jsx
@@ -1,46 +1,6 @@
-// import React from 'react';
-// import './Project.css';
-// import ProjectCard from './ProjectCard';
-
-// function Project() {
-//   const projects = [
-//     {
-//       name: 'Project One',
-//       description: 'A short description of project one. It’s an awesome project.',
-//       link: 'https://example.com/project-one',
-//     },
-//     {
-//       name: 'Project Two',
-//       description: 'Project two is even more amazing. Here’s a quick rundown.',
-//       link: 'https://example.com/project-two',
-//     },
-//     {
-//       name: 'Project Three',
-//       description: 'This is the third project. It’s cool and innovative!',
-//       link: 'https://example.com/project-three',
-//     },
-//   ];
-
-//   return (
-//     <div className="App">
-//       <h1 className="header">My Amazing Projects</h1>
-//       <div className="projects-container">
-//         {projects.map((project, index) => (
-//           <ProjectCard key={index} project={project} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Project;
-
-
-
-
 import React from 'react';
 import './Project.css';
-import ProjectCard from './ProjectCard';
+import ProjectCard from './Projectcard';
 
 function Project() {
   const projects = [
